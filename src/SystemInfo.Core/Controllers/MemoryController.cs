1f453a95adc9ded6e2f1d3e7c2447b7bc5f51db5
Thijs Bloebaum <thijs_bloebaum@hotmail.com>
2019-03-05 16:04:16 +0100

    Added Memory Controller

---
+++ src/SystemInfo.Core/Controllers/MemoryController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace SystemInfo.Core.Controllers
+{
+    public class MemoryController
+    {
+        public IObservable<UsageData> MemoryUse
+            => Observable
+            .Create<UsageData>(x => GetMemoryUseStream(x))
+            .Publish()
+            .RefCount();
+
+        private IDisposable GetMemoryUseStream(IObserver<UsageData> observer)
+        {
+            var disposables = new CompositeDisposable();
+            ulong totalVisibleMemorySize;
+
+            using (var mos_System = new ManagementObjectSearcher("select TotalVisibleMemorySize from Win32_OperatingSystem"))
+            {
+                totalVisibleMemorySize = (ulong)mos_System
+                    .Get()
+                    .Cast<ManagementObject>()
+                    .First()
+                    .Properties["TotalVisibleMemorySize"]
+                    .Value;
+            }
+
+            var availibleMemoryPerformanceCounter = new PerformanceCounter()
+            {
+                CategoryName = "Memory",
+                CounterName = "Available kBytes",
+                ReadOnly = true,
+            };
+
+            disposables.Add(availibleMemoryPerformanceCounter);
+            var updateTimer = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1))
+                .Select(_ => availibleMemoryPerformanceCounter.NextValue())
+                .Select(x => Convert.ToUInt64(x))
+                .Select(x => new UsageData(totalVisibleMemorySize, x))
+                .Subscribe(observer);
+            disposables.Add(updateTimer);
+
+            return disposables;
+        }
+    }
+}
---
+++ src/SystemInfo.Core/Controllers/UsageData.cs
@@ -0,0 +1,27 @@
+﻿namespace SystemInfo.Core.Controllers
+{
+    public class UsageData
+    {
+        public UsageData(ulong size, ulong available)
+        {
+            Size = size;
+            Available = available;
+        }
+
+        public ulong Available
+        {
+            get;
+        }
+
+        public int Percantage
+            => Utils.FloatToPercent((Size - Available) / Size);
+
+        public ulong Size
+        {
+            get;
+        }
+
+        public ulong TotalUsed
+            => Size - Available;
+    }
+}
