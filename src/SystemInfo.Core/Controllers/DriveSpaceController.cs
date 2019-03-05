fcf4d6f9651414e6f2889f6faf62aa7311796bee
Thijs Bloebaum <thijs_bloebaum@hotmail.com>
2019-03-05 17:07:27 +0100

    Add the drivespace controller

---
+++ src/SystemInfo.Core/Controllers/DriveSpaceController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace SystemInfo.Core.Controllers
+{
+    public class DriveSpaceController
+    {
+        public IObservable<IEnumerable<DriveSpaceData>> HDDUse
+            => Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(10))
+            .Select(_ => DriveInfo
+                .GetDrives()
+                .Where(d => d.IsReady && (d.DriveType == DriveType.Fixed || d.DriveType == DriveType.Network))
+                .Select(d => new DriveSpaceData(d.Name, (ulong)d.TotalSize, (ulong)d.AvailableFreeSpace)))
+            .Publish()
+            .RefCount();
+
+        public IObservable<UsageData> TotalHDDUse
+            => Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(10))
+            .Select(_ => GetTotalHDDUse())
+            .Publish()
+            .RefCount();
+
+        private UsageData GetTotalHDDUse()
+        {
+            var allDrives = DriveInfo
+                .GetDrives()
+                .Where(d => d.IsReady && (d.DriveType == DriveType.Fixed));
+
+            ulong totalSize = 0;
+            ulong totalAvailable = 0;
+
+            foreach (var drive in allDrives)
+            {
+                totalSize += (ulong)drive.TotalSize;
+                totalAvailable += (ulong)drive.AvailableFreeSpace;
+            }
+
+            return new UsageData(totalSize, totalAvailable);
+        }
+    }
+}
---
+++ src/SystemInfo.Core/Controllers/DriveSpaceData.cs
@@ -0,0 +1,16 @@
+﻿namespace SystemInfo.Core.Controllers
+{
+    public class DriveSpaceData : UsageData
+    {
+        public DriveSpaceData(string name, ulong size, ulong available)
+            : base(size, available)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+        }
+    }
+}
