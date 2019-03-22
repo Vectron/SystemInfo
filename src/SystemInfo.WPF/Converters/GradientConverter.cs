1410b64ec607fe953d3fb9016f90882e162f528f
Vectron <thijs_bloebaum@hotmail.com>
2019-03-22 15:51:25 +0100

    move the main item view to a template so we can reuse it for a preview in settings screen

---
+++ src/SystemInfo.WPF/Converters/GradientConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SystemInfo.WPF.Converters
+{
+    public class GradientConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var brush = new LinearGradientBrush();
+            brush.StartPoint = new Point(0, 0);
+            brush.EndPoint = new Point(1, 1);
+
+            brush.GradientStops.Add(new GradientStop((Color)values[0], 0));
+            brush.GradientStops.Add(new GradientStop((Color)values[1], 0.6));
+            brush.GradientStops.Add(new GradientStop((Color)values[2], 1));
+
+            return brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
