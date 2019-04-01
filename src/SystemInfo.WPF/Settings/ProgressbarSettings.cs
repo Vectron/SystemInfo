53a60608fa63e9861058e14c58fb836ff94c1932
Thijs Bloebaum <thijs_bloebaum@hotmail.com>
2019-04-01 22:57:50 +0200

    Change the settings to Colors for the progressbar instead of strings

--- src/SystemInfo.WPF/Settings/ProgressbarSettings.cs
+++ src/SystemInfo.WPF/Settings/ProgressbarSettings.cs
@@ -6,40 +6,40 @@ namespace SystemInfo.WPF.Settings
 {
     public class ProgressbarSettings : ObservableObject
     {
-        private string backgroundColor;
-        private string foregroundColorEnd;
-        private string foregroundColorMiddle;
-        private string foregroundColorStart;
+        private Color backgroundColor;
+        private Color foregroundColorEnd;
+        private Color foregroundColorMiddle;
+        private Color foregroundColorStart;
         private double height;
 
         public ProgressbarSettings()
         {
             Height = 5;
-            BackgroundColor = Colors.Gray.ToString();
-            ForegroundColorStart = Colors.LawnGreen.ToString();
-            ForegroundColorMiddle = Colors.Yellow.ToString();
-            ForegroundColorEnd = Colors.DarkRed.ToString();
+            BackgroundColor = Colors.Gray;
+            ForegroundColorStart = Colors.LawnGreen;
+            ForegroundColorMiddle = Colors.Yellow;
+            ForegroundColorEnd = Colors.DarkRed;
         }
 
-        public string BackgroundColor
+        public Color BackgroundColor
         {
             get => backgroundColor;
             set => SetField(ref backgroundColor, value);
         }
 
-        public string ForegroundColorEnd
+        public Color ForegroundColorEnd
         {
             get => foregroundColorEnd;
             set => SetField(ref foregroundColorEnd, value);
         }
 
-        public string ForegroundColorMiddle
+        public Color ForegroundColorMiddle
         {
             get => foregroundColorMiddle;
             set => SetField(ref foregroundColorMiddle, value);
         }
 
-        public string ForegroundColorStart
+        public Color ForegroundColorStart
         {
             get => foregroundColorStart;
             set => SetField(ref foregroundColorStart, value);
@@ -52,23 +52,21 @@ namespace SystemInfo.WPF.Settings
         }
 
         public override bool Equals(object obj)
-        {
-            return obj as ProgressbarSettings != null &&
-                   backgroundColor == (obj as ProgressbarSettings).backgroundColor &&
-                   foregroundColorEnd == (obj as ProgressbarSettings).foregroundColorEnd &&
-                   foregroundColorMiddle == (obj as ProgressbarSettings).foregroundColorMiddle &&
-                   foregroundColorStart == (obj as ProgressbarSettings).foregroundColorStart &&
-                   height == (obj as ProgressbarSettings).height;
-        }
+            => obj is ProgressbarSettings settings &&
+            BackgroundColor.Equals(settings.BackgroundColor) &&
+            ForegroundColorEnd.Equals(settings.ForegroundColorEnd) &&
+            ForegroundColorMiddle.Equals(settings.ForegroundColorMiddle) &&
+            ForegroundColorStart.Equals(settings.ForegroundColorStart) &&
+            Height == settings.Height;
 
         public override int GetHashCode()
         {
-            var hashCode = 831672693;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(backgroundColor);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(foregroundColorEnd);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(foregroundColorMiddle);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(foregroundColorStart);
-            hashCode = hashCode * -1521134295 + height.GetHashCode();
+            var hashCode = 927679445;
+            hashCode = hashCode * -1521134295 + EqualityComparer<Color>.Default.GetHashCode(BackgroundColor);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Color>.Default.GetHashCode(ForegroundColorEnd);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Color>.Default.GetHashCode(ForegroundColorMiddle);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Color>.Default.GetHashCode(ForegroundColorStart);
+            hashCode = hashCode * -1521134295 + Height.GetHashCode();
             return hashCode;
         }
     }
